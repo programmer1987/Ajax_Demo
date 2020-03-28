b6fc0aca5a348b81f1ee54e676fee8945d8e7a91
Serdar Kacar <34045346+programmer1987@users.noreply.github.com>
2020-03-28 21:46:52 +0100

    Add files via upload

---
+++ add_cars.php
@@ -0,0 +1,16 @@
+<?php
+include("db.php");
+
+if(isset($_POST['car_name'])) {
+
+  echo $car_name = $_POST['car_name'];
+  $query = "INSERT INTO cars(title) VALUES('$car_name') ";
+  $update_car_name = mysqli_query($connection, $query);
+
+  if(!$update_car_name) {
+    die("QUERY FAILED");
+  }
+  header("Location: index.html");
+}
+
+?>
---
+++ javascript.php
@@ -0,0 +1,8 @@
+<?php
+    if(isset($_POST)) {
+        echo $_POST['username'];
+        echo "<br>";
+        echo $_POST['password'];
+    }
+//    echo "YES is working";
+?>
